0e860a60a058404da49fad1dbd674bf563ff1348
Lezwon Castelino <lezwon@gmail.com>
2020-11-05 22:07:35 +0530

    added on_save to accelerators

--- pytorch_lightning/trainer/connectors/checkpoint_connector.py
+++ pytorch_lightning/trainer/connectors/checkpoint_connector.py
@@ -237,6 +237,7 @@ class CheckpointConnector:
         checkpoint = self.dump_checkpoint()
 
         model.on_hpc_save(checkpoint)
+        checkpoint = self.trainer.accelerator_backend.on_save(checkpoint)
 
         # do the actual save
         # TODO: fix for anything with multiprocess DP, DDP, DDP2
@@ -391,6 +392,7 @@ class CheckpointConnector:
 
         if self.trainer.is_global_zero:
             # write the checkpoint dictionary on the file
+            checkpoint = self.trainer.accelerator_backend.on_save(checkpoint)
             try:
                 atomic_save(checkpoint, filepath)
             except AttributeError as err:
